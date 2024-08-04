194daa96544b9f9f796f3d6bf7ab35d2bf685f03
Ryan Holanda <ryanholanda08@gmail.com>
2024-08-04 06:32:33 -0300

    feat: add support for izumi

--- src/contracts/ZupRouter.sol
+++ src/contracts/ZupRouter.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.26;
 
 import {UniswapRouter} from "src/contracts/UniswapRouter.sol";
+import {IzumiRouter} from "src/contracts/IzumiRouter.sol";
 
-contract ZupRouter is UniswapRouter {
-	constructor(address weth) UniswapRouter(weth) {}
+contract ZupRouter is UniswapRouter, IzumiRouter {
+	constructor(address weth) UniswapRouter(weth) IzumiRouter(weth) {}
 }
